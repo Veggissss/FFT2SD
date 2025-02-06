aedde739c5fb153ba798b5e7d326c2b7afea5ab5
Vegard Dæmring <44493351+Veggissss@users.noreply.github.com>
2025-02-06 21:33:05 +0100

    Replaced model_test template_json and glass with the generated model jsons.

--- model_test.py
+++ model_test.py
@@ -4,7 +4,7 @@ import json
 
 if __name__ == "__main__":
     # Define the model type and whether it is trained or not.
-    MODEL_TYPE = "decoder"
+    MODEL_TYPE = "encoder"
     IS_TRAINED = True
 
     # Load the test clinical data report data
@@ -13,6 +13,12 @@ if __name__ == "__main__":
     ) as f:
         test_json = json.load(f)
 
+    with open("data_model/out/generated-mikroskopisk.json", "r", encoding="utf-8") as f:
+        template_json = json.load(f)
+
+    with open("data_model/out/generated-beholder.json", "r", encoding="utf-8") as f:
+        container_json = json.load(f)
+
     # Add 'trained-' prefix to the model name if it is trained.
     model_key = f"trained-{MODEL_TYPE}" if IS_TRAINED else MODEL_TYPE
     model_loader = ModelLoader(MODELS_DICT[model_key], MODEL_TYPE)
@@ -20,35 +26,30 @@ if __name__ == "__main__":
     mask_token = (
         model_loader.tokenizer.mask_token
         if model_loader.tokenizer.mask_token is not None
-        else "[UTFYLL]"
+        else "null"
     )
-    glass_count_entry = {
-        "id": 0,
-        "field": "Antall glass",
-        "type": "int",
-        "value": f"{mask_token}",
-    }
+
+    # Mask out the total amount of containers present in the input text
+    container_json[0]["value"] = f"{mask_token}"
 
     # First find out how many containers/Beholder-IDs there are in the input text
     filled_json = model_loader.generate_filled_json(
         test_json["input_text"],
         "?",
-        json.dumps(glass_count_entry, ensure_ascii=False),
+        json.dumps(container_json[0], ensure_ascii=False),
     )
-    # print(filled_json)
 
     if filled_json.get("value") is not None:
         TOTAL_CONTAINERS = int(filled_json["value"])
         print(f"Container count: {TOTAL_CONTAINERS}")
     else:
-        print("Could not find the container count.")
+        print("ERROR: Could not find the container count!")
         TOTAL_CONTAINERS = 1  # DEBUG VALUE
 
     for container_number in range(TOTAL_CONTAINERS):
-        for template_entry in test_json["template_json"]:
+        for template_entry in template_json:
             template_entry["value"] = str(mask_token)
             template_str = json.dumps(template_entry, indent=2, ensure_ascii=False)
-            # print(template_str)
 
             # Generate filled JSON using the model
             filled_json = model_loader.generate_filled_json(
