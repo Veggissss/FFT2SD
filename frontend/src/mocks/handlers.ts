6060498ac53d4ebb0b22fe819191ae0a11e8ee42
Vegard Dæmring <44493351+Veggissss@users.noreply.github.com>
2025-05-10 20:30:05 +0200

    refactor: update mock handlers to use json files. Mocked /generate endpoint gives the requested report_type.

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,209 +1,68 @@
 import { http, HttpResponse } from "msw";
+import unlabeled_klinisk_mock from "./data/unlabeled_endpoint/klinisk_mock.json";
+import unlabeled_makroskopisk_mock from "./data/unlabeled_endpoint/makroskopisk_mock.json";
+import unlabeled_mikroskopisk_mock from "./data/unlabeled_endpoint/mikroskopisk_mock.json";
+import unlabeled_diagnose_mock from "./data/unlabeled_endpoint/diagnose_mock.json";
 
-const KLINISK_MOCK = {
-    "id": "16773557482994809422",
-    "is_diagnose": false,
-    "report_type": "klinisk",
-    "text": "3 mm polypp, oppfattet som hyperplastisk.\nCa 15 cm fra anus åpning (rektum/sigmo overgang).\nCRC i familien.\n\nAnatomisk lokalisasjon: \n1/1: sigmoideum\nRekvirent bekrefter at korrekt hastegrad er valgt\nEr prøven tatt i forbindels med tarmscreeningsprogrammet?: Ja"
-}
-
-const MAKROSKOPISK_MOCK = {
-    "id": "16773557482994809422",
-    "is_diagnose": false,
-    "report_type": "makroskopisk",
-    "text": "Mulig bredbaset polypp fra sigmoideum, 3x3x2 mm, hel i #1"
-}
-
-const MIKROSKOPISK_MOCK = {
-    "id": "6413961462004858133",
-    "is_diagnose": false,
-    "report_type": "mikroskopisk",
-    "text": "Tubulovilløst adenom med lavgradig dysplasi, colonbiopsi."
-}
-
-const DIAGNOSE_MOCK = {
-    "id": "4084497930299564480",
-    "is_diagnose": true,
-    "report_type": "mikroskopisk",
-    "text": "1: Ileumslimhinne med lange slanke totter og regelmessig kryptarkitektur. Upåfallende sylinderepitel. Vanlig mengde og fordeling av betennelsesceller i lamina propria, inkludert noen prominente lymfoide aggregat, slik man normalt kan finne i denne lokasjon. Ikke sett inflammasjon, granulomer eller dysplasi.\n\n2- 4 (prøve 2- 3): Mange biter colonslimhinne med polypoid fasong. Her ses tettliggende tubul\u00e6re og villøse strukturer, kledd av sylinderepitel med lett til moderat begercelletap og lett forstørrede, lett hyperkromatiske, basalstilte kjerner. Utseende overveiende som tubulovilløst adenom med lavgradig dysplasi. I snitt 4, fra prøve 3, ses et lite område med kribriform vekst, uttalt begercelletap, og betydelig cytologisk atypi med avrundede kjerner som viser tap av polaritet. Funnene passer med et lite foci med høygradig dysplasi."
-}
+import generated_klinisk_mock from "./data/generate_endpoint/klinisk_mock.json";
+import generated_makroskopisk_mock from "./data/generate_endpoint/makroskopisk_mock.json";
+import generated_mikroskopisk_mock from "./data/generate_endpoint/mikroskopisk_mock.json";
+import generated_diagnose_mock from "./data/generate_endpoint/diagnose_mock.json";
 
 export const handlers = [
     http.post(`*/load_model`, () => {
-        return HttpResponse.json(
-            { "success": true, "message": "MOCKED model loaded successfully! (Its not really loaded...)" }
-        )
+        return HttpResponse.json({
+            success: true,
+            message: "MOCKED model loaded successfully! (Its not really loaded...)"
+        });
     }),
 
-    http.get(`*/unlabeled/auto`, () => {
-        return HttpResponse.json(
-            KLINISK_MOCK
-        )
-    }),
-
-    http.get(`*/unlabeled/klinisk`, () => {
-        return HttpResponse.json(
-            KLINISK_MOCK
-        )
-    }),
-
-    http.get(`*/unlabeled/makroskopisk`, () => {
-        return HttpResponse.json(
-            MAKROSKOPISK_MOCK
-        )
-    }),
-
-    http.get(`*/unlabeled/mikroskopisk`, () => {
-        return HttpResponse.json(
-            MIKROSKOPISK_MOCK
-        )
-    }),
-
-    http.get(`*/unlabeled/diagnose`, () => {
-        return HttpResponse.json(
-            DIAGNOSE_MOCK
-        )
+    http.get(`*/unlabeled/:type`, ({ params }) => {
+        const type = params.type as string;
+        switch (type) {
+            case 'auto':
+            case 'klinisk':
+                return HttpResponse.json(unlabeled_klinisk_mock);
+            case 'makroskopisk':
+                return HttpResponse.json(unlabeled_makroskopisk_mock);
+            case 'mikroskopisk':
+                return HttpResponse.json(unlabeled_mikroskopisk_mock);
+            case 'diagnose':
+                return HttpResponse.json(unlabeled_diagnose_mock);
+            default:
+                return new HttpResponse(null, { status: 404 });
+        }
     }),
 
 
-    http.post(`*/generate`, () => {
-        return HttpResponse.json([
-            {
-                "input_text": "Mulig bredbaset polypp fra sigmoideum, 3x3x2 mm, hel i #1",
-                "metadata_json": [
-                    {
-                        "enum": [
-                            {
-                                "value": "klinisk"
-                            },
-                            {
-                                "value": "makroskopisk"
-                            },
-                            {
-                                "value": "mikroskopisk"
-                            }
-                        ],
-                        "field": "Rapport type",
-                        "id": 0,
-                        "type": "enum",
-                        "value": "makroskopisk"
-                    },
-                    {
-                        "field": "Antall glass",
-                        "id": 1,
-                        "type": "int",
-                        "unit": "stk",
-                        "value": 1
-                    },
-                    {
-                        "field": "Beholder-ID",
-                        "id": 128,
-                        "type": "int",
-                        "value": 1
-                    }
-                ],
-                "target_json": [
-                    {
-                        "enum": [
-                            {
-                                "name": "terminale ileum",
-                                "value": "T65520"
-                            },
-                            {
-                                "name": "coecum",
-                                "value": "T67100"
-                            },
-                            {
-                                "name": "colon ascendens",
-                                "value": "T67200"
-                            },
-                            {
-                                "name": "høyre colonfleksur",
-                                "value": "T67300"
-                            },
-                            {
-                                "name": "colon transversum",
-                                "value": "T67400"
-                            },
-                            {
-                                "name": "venstre colonfleksur",
-                                "value": "T67500"
-                            },
-                            {
-                                "name": "colon descendens",
-                                "value": "T67600"
-                            },
-                            {
-                                "name": "colon sigmoideum",
-                                "value": "T67700"
-                            },
-                            {
-                                "name": "rectosigmoid",
-                                "value": "T68200"
-                            },
-                            {
-                                "name": "rectum",
-                                "value": "T68000"
-                            },
-                            {
-                                "name": "anus",
-                                "value": "T69000"
-                            },
-                            {
-                                "name": "colon",
-                                "value": "T67000"
-                            },
-                            {
-                                "name": "colon og rectum",
-                                "value": "T67920"
-                            }
-                        ],
-                        "field": "Lokasjon patologi",
-                        "id": 107,
-                        "type": "enum",
-                        "value": "T67700"
-                    },
-                    {
-                        "enum": [
-                            {
-                                "name": "endoskopisk biopsi",
-                                "value": "P13400"
-                            },
-                            {
-                                "name": "endoskopisk slyngereseksjon",
-                                "value": "P13402"
-                            },
-                            {
-                                "name": "endoskopisk slyngereseksjon piecemeal",
-                                "value": "P13403"
-                            },
-                            {
-                                "name": "endoskopisk submucosal disseksjon",
-                                "value": "P13405"
-                            },
-                            {
-                                "name": "endoskopisk materiale, prosedyre ukjent",
-                                "value": "P13409"
-                            },
-                            {
-                                "name": "konsultasjonspreparat",
-                                "value": "P2070A"
-                            }
-                        ],
-                        "field": "Prøvemateriale",
-                        "id": 108,
-                        "type": "enum",
-                        "value": "P13409"
-                    },
-                    {
-                        "field": "Makroskopisk beskrivelse",
-                        "id": 109,
-                        "type": "string",
-                        "value": "3x3x2 mm, hel i #1"
-                    }
-                ]
-            }
-        ])
+    // Generation endpoint
+    http.post(`*/generate`, async ({ request }) => {
+        const body = await request.json() as Record<string, any>;
+        if (!body || !body.report_type || !body.input_text) {
+            return HttpResponse.json(
+                { "error": "Invalid request" },
+                { status: 400 }
+            );
+        }
+        const reportType: string = body.report_type;
+        const inputText: string = body.input_text;
+
+        // Mock data to return based on report type
+        let responseData;
+        switch (reportType.toLowerCase()) {
+            case 'klinisk':
+                responseData = generated_klinisk_mock;
+                break;
+            case 'makroskopisk':
+                responseData = generated_makroskopisk_mock;
+                break;
+            case 'mikroskopisk':
+                responseData = inputText === unlabeled_diagnose_mock.text ? generated_diagnose_mock : generated_mikroskopisk_mock;
+                break;
+            default:
+                responseData = { "error": `Unknown report type: ${reportType}` };
+        }
+        return HttpResponse.json(responseData);
     }),
 
     http.post(`*/correct/:reportId`, () => {
@@ -222,7 +81,6 @@ export const handlers = [
                     "Qwen/Qwen3-32B"
                 ],
                 "encoder": [
-                    "ltg/norbert3-small_mask_values",
                     "ltg/norbert3-small",
                     "ltg/norbert3-base",
                     "ltg/norbert3-large"
